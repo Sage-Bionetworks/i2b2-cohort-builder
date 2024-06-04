002322f14d27a9b7775f4665e8978596199b5856
Pranav Anbarasu <pranavanba@gmail.com>
2024-06-04 21:48:27 +0000

    Add pseudocode for next steps

--- python/main.py
+++ python/main.py
@@ -5,3 +5,8 @@ fpath = "participant_ids.csv"
 f = pd.read_csv(fpath, engine="pyarrow")
 
 participant_ids = list(set(f["PARTICIPANT_ID"].tolist()))
+
+# get list of datasets
+# for dataset in datasets_dir:
+  # temp_df = dataset.filter(ParticipantIdentifier in participant_ids)
+  # temp_df.write(format, output_dir)
