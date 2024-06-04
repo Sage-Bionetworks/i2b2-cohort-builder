5f4054a99c2a316206d71c1ed73e4e2320cb2101
Pranav Anbarasu <pranavanba@gmail.com>
2024-06-04 21:45:40 +0000

    Store unique values of PARTICIPANT_ID by converting to set then back to list

--- python/main.py
+++ python/main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-fpath = "/path/to/csv"
+fpath = "participant_ids.csv"
 
 f = pd.read_csv(fpath, engine="pyarrow")
 
-participant_ids = f["ParticipantIdentifier"].tolist()
+participant_ids = list(set(f["PARTICIPANT_ID"].tolist()))
