e8ac426707a29c4744828f2cd7cecb38008c124c
Pranav Anbarasu <pranavanba@gmail.com>
2024-06-11 22:10:39 +0000

    Get params from config file

--- python/main.py
+++ python/main.py
@@ -1,12 +1,22 @@
+import yaml
 import pandas as pd
+import os
 
-fpath = "participant_ids.csv"
+with open("config.yml", "r") as f:
+  config = yaml.safe_load(f)
+
+fpath = config["python"]["participants_csv_path"]
 
 f = pd.read_csv(fpath, engine="pyarrow")
 
 participant_ids = list(set(f["PARTICIPANT_ID"].tolist()))
 
-# get list of datasets
-# for dataset in datasets_dir:
-  # temp_df = dataset.filter(ParticipantIdentifier in participant_ids)
-  # temp_df.write(format, output_dir)
+source_dataset_path = config["python"]["source_dataset_path"]
+
+participant_partitions = os.listdir(source_dataset_path)
+
+selected_id_dirs = [dir for dir in participant_partitions if dir in participant_ids]
+
+destination_dataset_path = config["python"]["destination_dataset_path"]
+
+os.makedirs(os.path.join(destination_dataset_path, os.path.basename(source_dataset_path)))
