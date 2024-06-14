ac16c5502fb0644180bad894571c3f37f4cccace
Pranav Anbarasu <pranavanba@gmail.com>
2024-06-14 16:57:42 +0000

    Mimic main.py basic structure

---
+++ R/main.R
@@ -0,0 +1,40 @@
+#' Title
+#'
+#' @param path 
+#'
+#' @returns description
+#'
+#' @examples
+load_config <- function(path) {
+  
+}
+
+#' Title
+#'
+#' @param selected_participants_manifest_path 
+#' @param source_dataset_path 
+#'
+#' @returns description
+#'
+#' @examples
+get_cohort_partition_paths <- function(selected_participants_manifest_path, source_dataset_path) {
+  
+}
+
+#' Title
+#'
+#' @param selected_partitions_list 
+#'
+#' @returns description
+#'
+#' @examples
+build_cohort <- function(selected_partitions_list) {
+  
+}
+
+main <- function() {
+  
+}
+
+main()
+
